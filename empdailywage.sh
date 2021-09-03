a917c2c49d4617fa12ad5444def3b37e78354639
HarshithaG069 <harshithag069@gmail.com>
2021-09-03 08:41:30 +0530

    empdailywage

---
+++ empdailywage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+EmpPresent=1
+EmpAbsent=0
+WagePerHr=20
+check=$((RANDOM%2))
+if [ $check  -eq 1 ]
+then
+FullDayHr=8
+else
+FullDayHr=0
+fi
+echo " employee daily wage is = $(($FullDayHr*$WagePerHr)) "
